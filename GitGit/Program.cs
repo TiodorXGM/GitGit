2a2916a64d0e39714fa9b3521113a9244680baa0
Tiodor <tiodor.xgm.ru@gmail.com>
2024-11-18 16:47:43 +0200

    Add power method

--- GitGit/Program.cs
+++ GitGit/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,25 @@ namespace GitGit
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
+            Console.WriteLine("Введите число:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите в какую степень нужно возвести число:");
+            int exp = Convert.ToInt32(Console.ReadLine());
+
+            int result = program.Power(number, exp);
+            Console.WriteLine($"Результат: {result}");
+
+            Console.ReadKey();
 
 
         }
+
+        int Power(int number, int exponent)
+        {
+            return (int)Math.Pow(number, exponent); 
+        }
     }
 }
+
